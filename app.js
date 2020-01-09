b590524c392eae4f02ec18bde84fa6f78d7691a5
sangbum <bum922@gmail.com>
2020-01-09 16:44:45 +0900

    add cors middleware

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const DBconfig = require('./config/DB.js')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-
-mongoose.Promise = global.Promise
-mongoose.connect(DBconfig.DB, {useNewUrlParser: true, useUnifiedTopology: true}).then(
-  () => {console.log(`Database is connected`)},
-  err => {console.log(`Can not connect to the database ${err}`)}
-)
 
 const app = express()
 
@@ -20,6 +16,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+// to allow CORS(Cross Origin Resource Sharing)
+app.use(cors())
+
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
@@ -27,6 +26,13 @@ app.use('/users', usersRouter)
 app.use(function(req, res, next) {
   next(createError(404))
 })
+
+// init mongoose connection
+mongoose.Promise = global.Promise
+mongoose.connect(DBconfig.DB, {useNewUrlParser: true, useUnifiedTopology: true}).then(
+  () => {console.log(`Database is connected`)},
+  err => {console.log(`Can not connect to the database ${err}`)}
+)
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,16 @@ let User = require('../models/user.model')
 
 router.route('/user')
   .post((req, res) => {
-    console.log(req.body)
     let user = new User(req.body)
     user.save()
       .then(() => {
-        res.status(200).json({'business': 'business in added successfully'})
+        res.status(200).send(`user add success.`)
       })
       .catch(() => {
         res.status(400).send(`user add fail.`)
       })
   })
   .get((req, res) => {
-    console.log(req.body)
     User.find((err, users) => {
       if (err) {
         res.json(err)
